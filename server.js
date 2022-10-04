22da8d77082e3464340bcea53ed07b8b16d0fa5c
RakeebNazar <mohamednazar032@gmail.com>
2022-10-04 09:12:29 +0530

    Initial Commit

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose
   })
   .then(() => console.log("DB connection successful!"));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`running`);
 });
